f06f31eb8b2cbf0513dcc66935f668944e98ab54
SandyArdiyan <sanardiyan881@gmail.com>
2025-05-13 20:30:00 +0700

    first commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "block";
+});
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
